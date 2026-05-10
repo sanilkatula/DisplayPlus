1be261968e9e71319560ea01674338c96541b3ac
sanilkatula <124859948+sanilkatula@users.noreply.github.com>
2026-05-09 23:51:54 -0700

    Initial Commit

---
+++ Display+/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Display+
+//
+//  Created by SANIL KATULA on 5/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Display+/Display_App.swift
@@ -0,0 +1,17 @@
+//
+//  Display_App.swift
+//  Display+
+//
+//  Created by SANIL KATULA on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Display_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
